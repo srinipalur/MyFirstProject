34d2c69d508eac14bf6478e4966eb732189f4623
srinipalur <srinipalur@gmail.com>
2021-09-06 00:44:02 -0400

    Hello World

--- TestModule/src/Test.java
+++ TestModule/src/Test.java
@@ -1,5 +1,18 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int count,prime_sum=1;
+        for(int i=3;i<100;i+=2)
+        {
+            count=0;
+            for(int j=3;j<=i/2;j++){
+                if(i%j==0)
+                    count++;
+            }
+            if(count==0) {
+                System.out.println(i);
+                prime_sum++;
+            }
+        }
+        System.out.println(prime_sum);
     }
 }
